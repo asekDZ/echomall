ae427ea02555e1594a828e22317e5af823a8771c
Adel Salah Eddine <42499474+asekDZ@users.noreply.github.com>
2019-07-06 16:09:47 +0100

    VERSION1.0 BETA

---
+++ app/src/main/java/dz/uc2/sci/echomall/SignUpFragment.java
@@ -0,0 +1,60 @@
+package dz.uc2.sci.echomall;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SignUpFragment extends Fragment {
+
+
+    public SignUpFragment() {
+        // Required empty public constructor
+    }
+
+    private TextView alreadyHaveAccount;
+    private FrameLayout parentFrameLayout;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        alreadyHaveAccount = view.findViewById(R.id.tv_sign_in);
+        parentFrameLayout= getActivity().findViewById(R.id.register_framelayout);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        alreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFragment(new SignInFragement());
+            }
+        });
+
+    }
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.slide_from_left,R.anim.slideout_from_right);
+        fragmentTransaction.replace(parentFrameLayout.getId(),fragment);
+        fragmentTransaction.commit();
+
+
+        }
+    }
